f2512a51046a245d6b536fc3b6aec63ac78b2e4d
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-04-27 01:13:53 +0430

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#25252D',
+        purple: '#7047EA',
+        green: '#2DCA72',
+        gray: '#9696A7',
+        cool: '#E6E6EA',
+        light: '#F1F1F4',
+        white: '#FFFFFF',
+      },
+    },
   },
   variants: {
     extend: {},
