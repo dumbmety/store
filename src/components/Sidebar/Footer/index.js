6e65c17436c1fe5e0a3a38e894f897b42de5f1f3
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-05-01 00:49:26 +0430

    added instagram link to footer

--- src/components/Sidebar/Footer/index.js
+++ src/components/Sidebar/Footer/index.js
@@ -5,7 +5,13 @@ import { DotsHorizontalIcon } from '@heroicons/react/outline'
 function Footer() {
   return (
     <footer className="main-footer">
-      <img className="logo" src="/images/team-logo.png" alt="Junior Coders" />
+      <a
+        href="https://www.instagram.com/junior.coders"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="logo" src="/images/team-logo.png" alt="Junior Coders" />
+      </a>
       <h4 className="name">Junior Coders Team</h4>
       <button className="button">
         <DotsHorizontalIcon width={24} height={24} />
