4f67a32e5446b382980a77ce0cb760c51d33c734
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-05-04 00:57:16 +0430

    added product detail page

---
+++ src/components/Product/index.js
@@ -0,0 +1,35 @@
+import './index.css'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import SimpleBar from 'simplebar-react'
+
+import products from '../../constants/products'
+
+function Product({ match }) {
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    setProduct(products.find(p => p.route === match.params.name))
+  }, [])
+
+  return (
+    <SimpleBar className="max-h-screen">
+      <div className="product-wrapper">
+        <div className="image">
+          <img src={`/images/${product.image}`} alt={product.name} />
+        </div>
+        <div className="details">
+          <Link className="link" to="/">
+            back to home
+          </Link>
+          <h1 className="name">{product.name}</h1>
+          <span className="price">${product.price}</span>
+          <p className="description">{product.description}</p>
+          <button className="add-to-cart">Add to cart</button>
+        </div>
+      </div>
+    </SimpleBar>
+  )
+}
+
+export default Product
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './Home'
+import Product from '../components/Product'
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/product/:name" component={Product} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Router>
