134ae703d2ab1351c4e884034640aa34558f5ca9
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-06-06 13:17:58 +0430

    Update: Add redux & react-redux

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import ReactDOM from 'react-dom'
 import App from './containers/App'
 import 'simplebar/dist/simplebar.min.css'
 import './styles/index.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const initialState = ['test']
+
+const reducer = (state = initialState, action) => {
+  return state
+}
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
